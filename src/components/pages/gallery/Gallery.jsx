5dc9d397db60abda55d17e7566bf18dafeccb5d0
AadeshJuvekar <juvekaraadesh66@gmail.com>
2022-03-12 15:44:52 +0530

    Landing Page Updated

---
+++ src/components/pages/gallery/Gallery.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './gallery.css'
+function Gallery() {
+  return (
+   <div className="gallery">
+       <div className="galleryContainer">
+           
+       </div>
+   </div>
+  )
+}
+
+export default Gallery
--- src/components/pages/landing/Landing.jsx
+++ src/components/pages/landing/Landing.jsx
@@ -6,7 +6,9 @@ function Landing() {
     <div className="landing">
       <div className="landingContainer">
         <div className="landingBody">
-          <h1 className="landingText">Beach Camping System </h1>
+          <h1 className="landingText">Lorem, ipsum dolor sit amet consectetur adipisicing elit. </h1>
+          <button className="landingButton">Packages</button>
+          <button className="landingButton">Services</button>
         </div>
       </div>
     </div>
